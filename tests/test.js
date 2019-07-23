f958d8b1e42cb9a0a64e90cec3a73ba2ef43d86b
Piepongwong <j.tonneyck@gmail.com>
2019-07-23 17:06:51 +0200

    added test.js

--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,6 @@ var app = require('../index.js');
 describe('GET /', function() {
  it('respond with hello world', function(done) {
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world', done);
+ request(app).get('/').expect('hello world', done());
  });
 });
