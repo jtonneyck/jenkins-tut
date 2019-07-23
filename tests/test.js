73abe1462ec27db5f951f95dd0d43206db06b0da
Piepongwong <j.tonneyck@gmail.com>
2019-07-23 23:17:08 +0200

    broke a test

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,6 @@ var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
  it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
+    request(app).get('/').expect('hello worlddd', done);
  });
 });
